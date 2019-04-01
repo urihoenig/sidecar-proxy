e81379ba79b34cc549f4b2c611b512ed9cac41f2
Pavel <pavelr3@gmail.com>
2019-04-01 19:08:27 +0300

    Configurable log level

--- main.go
+++ main.go
@@ -14,11 +14,16 @@ func main() {
 	// args
 	listenAddress := flag.String("listen-addr", os.Getenv("PROXY_LISTEN_ADDRESS"), "Port to listen on")
 	forwardAddress := flag.String("forward-addr", os.Getenv("PROXY_FORWARD_ADDRESS"), "IP /w port to forward to (without protocol)")
+	logLevel := flag.String("log-level", os.Getenv("LOG_LEVEL"), "Set proxy's log level")
 	flag.Parse()
 
 	// logger conf
 	var logger = logrus.New()
-	logger.SetLevel(logrus.DebugLevel)
+	parsedLogLevel, err := logrus.ParseLevel(*logLevel)
+	if err != nil {
+		panic(err)
+	}
+	logger.SetLevel(parsedLogLevel)
 
 	proxyServer, err := app.CreateProxyServer(logger, *listenAddress, *forwardAddress)
 	if err != nil {
